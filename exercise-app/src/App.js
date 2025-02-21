5494a85a75dc3d72ad4988df6e5e4262ae74f1c4
Chloe <chloechina04@aol.com>
2025-02-21 13:40:16 -0500

    Copied from previous assignment

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import Duration from "./components/DurationExercise";
+import Repetition from "./components/RepetitionExercise";
 import './App.css';
+import React, { useState } from 'react';
+
+// data
+let exerciseData = [
+  {
+    name: "Pushups",
+    type: "Repetition",
+    description: "20 Reps - 2 Sets",
+  },
+  {
+    name: "Runs",
+    type: "Duration",
+    description: "30 mins",
+  },
+  {
+    name: "Squats",
+    type: "Repetition",
+    description: "20 Reps - 2 Sets",
+  },
+
+];
 
 function App() {
+  // choice checker
+  const [selectedExercise, setSelectedExercise] = useState(null);
+
+  // get exercise
+  const button = (exercise) => {
+    setSelectedExercise(exercise);
+  };
+
+  // if null, dont go anywhere else go based on exercise type
+  let content;
+  if (selectedExercise == null) {
+    content = (
+      <div className="main-page-buttons">
+        <h1>Do Some Exercise</h1>
+        <h3>Recommended Exercises</h3>
+        {exerciseData.map((exercise, index) => (
+          <button key={index} onClick={() => button(exercise)}>
+            <div>{exercise.name}</div>
+            <div>{exercise.description}</div>
+          </button>
+        ))}
+      </div>
+    );
+  } else {
+    // go different places depending on exercise type
+    if (selectedExercise.type === "Duration") {
+      content = <Duration name={selectedExercise.name} />;
+    } else if (selectedExercise.type === "Repetition") {
+      content = <Repetition name={selectedExercise.name} />;
+    }
+  }
+
+  // display to screen
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {content}
     </div>
   );
 }
