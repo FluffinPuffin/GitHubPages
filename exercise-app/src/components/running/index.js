0c40ba6132b5ceda65c31926ba9299f2ba7e15df
Chloe <chloechina04@aol.com>
2025-02-21 14:12:17 -0500

    need to fix array later

--- exercise-app/src/components/running/index.js
+++ exercise-app/src/components/running/index.js
@@ -1,33 +1,51 @@
 import React, { useState } from 'react';
 
-function RepetitionExercise({ name }) {
+function RunningExercise({ name }) {
     // Set useState
+    let array = [];
     const [count, setCount] = useState(0);
     const [currentTime, setCurrentTime] = useState('');
 
     // Add by 1
     const handleIncrement = () => {
-        setCount(count + 1);
         const now = new Date();
         const formattedTime = now.toLocaleTimeString();
         setCurrentTime(formattedTime);
+        setCount(count + 1);
+        array.push(count + formattedTime);
     };
 
     // Reset to 0
-    const handleReset = () => {
+    const handleReset = (array) => {
         setCount(0);
         setCurrentTime('');
+        for (let i = 0; i < array.length; i++) {
+            array.pop();
+        }
     };
 
-    // Display this to screen
+    const updateArray = (array) => {
+        if (array === undefined) {
+        } else {
+            const listLaps = array.map((item) =>
+                <li>{item}</li>
+            );
+            return (
+                <ul>{listLaps}</ul>
+            );
+        }
+    };
+
+    // Display this to screen <div>Lap: {count} - Current Time: {currentTime}</div>
     return (
         <div class="running">
             <h2>{name}</h2>
-            <div>Lap: {count} - Current Time: {currentTime}</div>
             <button onClick={handleIncrement}>Lap</button>
             <button onClick={handleReset}>Reset</button>
+            <div>Laps: Time: </div>
+            {updateArray()}
         </div>
     );
 }
 
-export default RepetitionExercise;
+export default RunningExercise;
