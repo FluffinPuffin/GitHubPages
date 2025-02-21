65aabbd0641211ab3f9ccd9b0252ad6f2e95aae6
Chloe <chloechina04@aol.com>
2025-02-21 13:54:17 -0500

    Got it to recod time

--- exercise-app/src/components/running/index.js
+++ exercise-app/src/components/running/index.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
 
-function RuningExercise({ name }) {
+function RepetitionExercise({ name }) {
     // Set useState
     const [count, setCount] = useState(0);
+    const [currentTime, setCurrentTime] = useState('');
 
     // Add by 1
     const handleIncrement = () => {
         setCount(count + 1);
+        const now = new Date();
+        const formattedTime = now.toLocaleTimeString();
+        setCurrentTime(formattedTime);
     };
 
     // Reset to 0
     const handleReset = () => {
         setCount(0);
+        setCurrentTime('');
     };
 
     // Display this to screen
     return (
-        <div class="repetition">
+        <div class="running">
             <h2>{name}</h2>
-            <div>Count: {count}</div>
-            <button onClick={handleIncrement}>Increase</button>
+            <div>Lap: {count} - Current Time: {currentTime}</div>
+            <button onClick={handleIncrement}>Lap</button>
             <button onClick={handleReset}>Reset</button>
         </div>
     );
 }
 
-export default RuningExercise;
+export default RepetitionExercise;
